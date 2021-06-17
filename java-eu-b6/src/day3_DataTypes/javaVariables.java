0a39d183419e181ea7e9f9ed62ae9bbb0fc35173
muhammet-cakmak <cakmakmuhammet12@gmail.com>
2021-06-17 20:42:57 +0200

    Arithmetic operators has been added

--- java-eu-b6/src/day3_DataTypes/javaVariables.java
+++ java-eu-b6/src/day3_DataTypes/javaVariables.java
@@ -4,6 +4,27 @@ public class javaVariables {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Declaration
+		byte inches;
+		int speed;
+		short month;
+		float salesCommision;
+		double distance;
+		
+		//assigned
+		inches=5;
+		speed=200;
+		month=2;
+		salesCommision=5;
+		distance=40.2;
+		
+		//declared+assigned
+		
+		byte inches2=5;
+		
+		
+		System.out.println("My speed is " + speed);
 
 	}
 
---
+++ java-eu-b6/src/day4_arithmeticOperators/arithmeticOperators.java
@@ -0,0 +1,10 @@
+package day4_arithmeticOperators;
+
+public class arithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
